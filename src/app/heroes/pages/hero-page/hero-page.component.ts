e6ba91fc57918ba34acfe75f777f11b293396bd7
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2024-07-24 18:39:36 -0500

    Obtener Héroe por URL (params)
    Obtener Héroe por URL (params)

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/heros.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+
+public hero? : Hero;
+
+  constructor(
+  private heroesSrevice: HeroService,
+  private activatedRoute: ActivatedRoute,
+  private router: Router,
+){}
+
+
+  ngOnInit(): void {
+  this.activatedRoute.params
+  .pipe(
+    switchMap(({id}) => this.heroesSrevice.getHeroById(id)),
+  )
+  .subscribe( hero => {
+    if (!hero ) return this.router.navigate(['/heroes/list']);
+
+    this.hero = hero;
+    return;
+
+  })
+  }
 
 }
