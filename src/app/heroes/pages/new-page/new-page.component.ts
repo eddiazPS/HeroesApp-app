f2bbbfba8a181f4f0a7202cfa318401d69765f04
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2024-07-25 17:25:36 -0500

    Getters dentro del componente (get currentHero)

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/heros.service';
 
 @Component({
   selector: 'app-new-page',
@@ -9,28 +10,32 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
-public heroForm = new FormGroup ({
-  id:        new FormControl<string>(''),
-  superhero: new FormControl<string>('',{nonNullable: true}),
-  publisher: new FormControl<Publisher>(Publisher.DCComics),
-  alter_ego: new FormControl(''),
-  first_appearance: new FormControl(''),
-  characters: new FormControl(''),
-  alt_img:   new FormControl(''),
-  DCComics:  new FormControl(''),
-});
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+    DCComics: new FormControl(''),
+  });
 
   public publishers = [
-    {id:'DC Comics', desc:'DC - Comics'},
-    {id:'Marvel Comics', desc:'Marvel - Comics'},
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ]
 
-  onSubmit():void {
+  constructor(private heroService: HeroService) { }
 
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value,
-    })
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
 
   }
 
